1a10654bf0eee8eb4d2da64842f7bf679dc7612a
Urban Dopudja <urban.dopudja@gmail.com>
2024-05-04 19:44:22 +0200

    added default configuration file and script now reads the config

--- LLMNRAutomation.sh
+++ LLMNRAutomation.sh
@@ -1,2 +1,17 @@
-test
-update
+#!/bin/bash
+
+# Update user preferences
+# Path to the configuration files
+LLMNRAutomation_conf="LLMNRAutomation.conf"
+Responder_conf="/usr/share/responder/Responder.conf"
+
+# Read the LLMNRAutomation.conf file
+while IFS='=' read -r setting value; do
+    setting=$(echo "$setting" | tr -d '[:space:]')
+    value=$(echo "$value" | tr -d '[:space:]')
+
+    if grep -q "^$setting" "$Responder_conf"; then
+        sed -i "s/^$setting.*$/& = $value/" "$Responder_conf"
+    fi
+done < "$LLMNRAutomation_conf"
+
